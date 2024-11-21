9738f19209956fa59b00b8c975edb32f43e3e6d7
JangSeung <0sunghee122@gmail.com>
2024-11-21 18:08:16 +0900

    .

---
+++ hamsu.py
@@ -0,0 +1,23 @@
+# 전역 변수, 지역 변수
+# a = 1
+# def ha(a):
+#     a = a + 1
+#     return a
+
+# print(ha(a))
+
+
+# 전역 변수, 지역 변수
+# a = 1
+# def ha(a):
+#    global a      # global : 전역 변수 끌어오기
+#    a = a+1
+
+# ha(a)
+# print(a)
+
+
+a = []
+
+
+    
